677c0ae12a6f4a91f3685346f57af564906f972b
Rod Johnson <rod@atomist.com>
2017-11-17 17:08:59 +1100

    Further Java tests

---
+++ test/tree/ast/antlr/java/editJavaTest.ts
@@ -0,0 +1,36 @@
+import "mocha";
+import * as assert from "power-assert";
+
+import { InMemoryProject } from "@atomist/automation-client/project/mem/InMemoryProject";
+import { findFileMatches } from "@atomist/automation-client/tree/ast/astUtils";
+import { JavaFileParser } from "../../../../../src/tree/ast/antlr/java/JavaFileParser";
+
+import { AllJavaFiles } from "@atomist/automation-client/operations/generate/java/javaProjectUtils";
+
+describe("java review/edit", () => {
+
+    it("should find and remove unnecessary component scan annotation", done => {
+        const path = "src/main/java/Foo.java";
+        const content = "public @SpringBootApplication @ComponentScan class MyApp {}";
+        const p = InMemoryProject.of(
+            {path, content});
+        // TODO use zapAllMatches
+        findFileMatches(p, JavaFileParser, AllJavaFiles,
+            `//typeDeclaration[/classDeclaration]
+                 [//annotation[@value='@SpringBootApplication']]
+                 //annotation[@value='@ComponentScan']`)
+            .then(fm => {
+                assert(fm.length === 1);
+                const target = fm[0];
+                target.matches[0].$value = "";
+                p.flush().then(_ => {
+                    const f = p.findFileSync(path);
+                    assert(f.getContentSync() === content.replace("@ComponentScan", ""),
+                        `Erroneous content: [${f.getContentSync()}]`);
+                    done();
+                });
+            }).catch(done);
+    });
+
+    it("should remove input");
+});
